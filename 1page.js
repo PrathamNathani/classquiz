d13592d8017e74ab2be58016fc5f738f9f321cfc
PrathamNathani <73383584+PrathamNathani@users.noreply.github.com>
2021-05-04 22:00:41 +0530

    Add files via upload

---
+++ 1page.js
@@ -0,0 +1,7 @@
+function adduser(){
+    var player1_name=document.getElementById("player_1_name").value;
+    var player2_name=document.getElementById("player_2_name").value;
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    window.location="index.html";
+}
